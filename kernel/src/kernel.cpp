1b757f9df638e62429a0f68a660a89565081a340
Wayne Thornton <wmthornton@outlook.com>
2021-06-02 10:15:35 -0600

    Changed licensing scheme to GPLv3

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -1,3 +1,20 @@
+/**
+ * Copyright (c) 2021 Wayne Michael Thornton <wmthornton>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
 #include "Kernel_Util.h"
 
 // We use extern "C" to tell the file that it is being called from an external C program file
